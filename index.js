3eb4fd0c374f6e134dc6121a5f812831bd5e5220
Bryan Lee <bryanl@genustechnologies.com>
2024-08-15 16:20:24 -0500

    added set options back

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function DisplayCameraUI() {
     // User has to select a document type
     const select = document.getElementById('document_select');
     if (select.value == 0) {
-        alert('Select a Document Type');
+        document.getElementById('error_message').innerHTML = 'Error: Select a Document Type';
 
         return;
     }
@@ -59,74 +59,74 @@ function DisplayCameraUI() {
         defaultOptions.preference = "camera";
         defaultOptions.useVideoStream = true;
         defaultOptions.preview = true;
-        defaultOptions.framePadding = 14;
+        // defaultOptions.framePadding = 14;
 
-        const aspectRatios = {
-            1: { ratio: 0.46, message: 'User selected Check' },
-            2: { ratio: 0.629, message: 'User selected MobileId' },
-            3: { ratio: 0.703, message: 'User selected Passport' },
-            4: { ratio: 0.623, message: 'User selected Credit Card' },
-            5: { ratio: 0.615, message: 'User selected Pay Bill' }
-        };
+        // const aspectRatios = {
+        //     1: { ratio: 0.46, message: 'User selected Check' },
+        //     2: { ratio: 0.629, message: 'User selected MobileId' },
+        //     3: { ratio: 0.703, message: 'User selected Passport' },
+        //     4: { ratio: 0.623, message: 'User selected Credit Card' },
+        //     5: { ratio: 0.615, message: 'User selected Pay Bill' }
+        // };
 
-        const selectedOption = aspectRatios[select.value];
-        if (selectedOption) {
-            defaultOptions.frameAspectRatio = selectedOption.ratio;
-            console.info(selectedOption.message);
-            document.getElementById('error_message').innerHTML = selectedOption.message;
-        }
+        // const selectedOption = aspectRatios[select.value];
+        // if (selectedOption) {
+        //     defaultOptions.frameAspectRatio = selectedOption.ratio;
+        //     console.info(selectedOption.message);
+        //     document.getElementById('error_message').innerHTML = selectedOption.message;
+        // }
 
         // Initialize the capture control with default options
         KfxWebSDK.Capture.create(defaultOptions, function(createSuccess) {
             console.info('Capture control created successfully:', createSuccess);
             document.getElementById('error_message').innerHTML = 'Capture control created successfully';
 
-            // Set additional capture options if needed
-            var captureOptions = {
-                useTargetFrameCrop: false,
-                frameAspectRatio: 0.628,
-                framePadding: 5,
-                frameCornerHeight: 15,
-                frameCornerWidth: 70,
-                frameCornerColor: '#00FF00',
-                resolution: KfxWebSDK.resolution.RES_FULL_HD,
-                downscaleSize: 2,
-                outOfFrameTransparency: 0.5,
-                showEdges: false,
-                edgesColor: '#FFFF00',
-                edgesWidth: 4,
-                enableFlashCapture: false,
-                guidanceSize: 150,
-                criteria: {
-                    captureTimeout: 1700,
-                    centerToleranceFraction: 0.15,
-                    longAxisThreshold: 85,
-                    shortAxisThreshold: 60,
-                    maxFillFraction: 1.8,
-                    minFillFraction: 0.65,
-                    turnSkewAngleTolerance: 10,
-                    pitchThreshold: 15,
-                    rollThreshold: 15
-                },
-                lookAndFeel: {
-                    // documentSample: 'http://example.com/images/document_sample.jpg',
-                    showTapToDismissMessage: true,
-                    forceCapture: 10,
-                    gallery: true
-                }
-            };
+            // // Set additional capture options if needed
+            // var captureOptions = {
+            //     useTargetFrameCrop: false,
+            //     frameAspectRatio: 0.628,
+            //     framePadding: 5,
+            //     frameCornerHeight: 15,
+            //     frameCornerWidth: 70,
+            //     frameCornerColor: '#00FF00',
+            //     resolution: KfxWebSDK.resolution.RES_FULL_HD,
+            //     downscaleSize: 2,
+            //     outOfFrameTransparency: 0.5,
+            //     showEdges: false,
+            //     edgesColor: '#FFFF00',
+            //     edgesWidth: 4,
+            //     enableFlashCapture: false,
+            //     guidanceSize: 150,
+            //     criteria: {
+            //         captureTimeout: 1700,
+            //         centerToleranceFraction: 0.15,
+            //         longAxisThreshold: 85,
+            //         shortAxisThreshold: 60,
+            //         maxFillFraction: 1.8,
+            //         minFillFraction: 0.65,
+            //         turnSkewAngleTolerance: 10,
+            //         pitchThreshold: 15,
+            //         rollThreshold: 15
+            //     },
+            //     lookAndFeel: {
+            //         // documentSample: 'http://example.com/images/document_sample.jpg',
+            //         showTapToDismissMessage: true,
+            //         forceCapture: 10,
+            //         gallery: true
+            //     }
+            // };
 
-            KfxWebSDK.Capture.setOptions(captureOptions, function() {
-                console.info('Capture options set successfully.');
-                document.getElementById('error_message').innerHTML = 'Capture options set successfully';
+            // KfxWebSDK.Capture.setOptions(captureOptions, function() {
+            //     console.info('Capture options set successfully.');
+            //     document.getElementById('error_message').innerHTML = 'Capture options set successfully';
 
-            }, function(error) {
-                console.error('Error setting capture options:', error);
-                document.getElementById('error_message').innerHTML = 'Error setting capture options: ' + error.message;
+            // }, function(error) {
+            //     console.error('Error setting capture options:', error);
+            //     document.getElementById('error_message').innerHTML = 'Error setting capture options: ' + error.message;
 
-            });
+            // });
 
-            setTimeout(takePic, 1000);            
+            // setTimeout(takePic, 1000);            
         }, function(error) {
             console.error('Error creating capture control:', error);
             document.getElementById('error_message').innerHTML = 'Error creating capture control: ' + error.message;

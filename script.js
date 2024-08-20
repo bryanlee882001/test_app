c58adb8585b8140a4ed12c0ae9be20bf5e489c83
Bryan Lee <bryanl@genustechnologies.com>
2024-08-20 11:07:55 -0500

    prev working, testing display for cameraContainer

--- script.js
+++ script.js
@@ -66,6 +66,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Capture image on button click
     document.getElementById('captureButton').addEventListener('click', function() {
+        document.getElementById('cameraContainer').style.display = 'block';
+        
         KfxWebSDK.Capture.takePicture(function(imageData) {
             console.info('Image captured successfully:', imageData);
 
